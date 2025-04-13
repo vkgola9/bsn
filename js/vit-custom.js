786a316cbcf54f9ff5ae8d16c252898771ad2758
vkgola9 <120744720+vkgola9@users.noreply.github.com>
2025-04-13 22:24:44 +0530

    Add files via upload

---
+++ js/vit-custom.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+//   $(".caro1").owlCarousel({
+//     loop: true,
+//     autoplay: true,
+//     autoTimeOut: 1000,
+//     items: 1,
+//     nav: false,
+//     dots: false,
+//     // lazyLoad: true,
+//     autoplayHoverPause: false,
+//     // animateOut: "slideInDown",
+//     // animateIn: "fadeOut",
+//     // onInitialize: resetProgressBar,
+//     // onInitialized: resetProgressBar,
+//     // onTranslate: resetProgressBar,
+//     // onTranslated: startProgressBar,
+//     touchDrag: true,
+//     mouseDrag: true,
+//     navText: [
+//       "<i class='fa-solid fa-chevron-left'></i>",
+//       "<i class='fa-solid fa-chevron-right'></i>",
+//     ],
+//   });
+//   function startProgressBar() {
+//     // apply keyframe animation
+//     $(".slide-progress").css({
+//       width: "100%",
+//       transition: "width 5000ms"
+//     });
+//   }
+//   function resetProgressBar() {
+//     $(".slide-progress").css({
+//       width: 0,
+//       transition: "width 0s"
+//     });
+//   }
+
+
+   // swiper slider
+   var thumb = new Swiper('.mySwiper', {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 8,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2
+      },
+      340: {
+        slidesPerView: 4
+      },
+      640: {
+        slidesPerView: 6
+      },
+      1024: {
+        slidesPerView: 7
+      }
+    }
+  })
+  var swiper2 = new Swiper('.mySwiper2', {
+    loop: true,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2000
+    },
+    effect: 'fade',
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94
+    },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev'
+    // },
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    //   renderBullet: function (index, className) {
+    //     return '<span class="' + className + '">' + (index + 1) + '</span>'
+    //   }
+    // },
+    thumbs: {
+      swiper: thumb
+    }
+  })
+});
