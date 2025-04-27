10994ba07d2a1f8593604dfde7b93fd158a4a9c6
vkgola9 <120744720+vkgola9@users.noreply.github.com>
2025-04-27 15:44:43 +0530

    Add files via upload

--- js/vit-custom.js
+++ js/vit-custom.js
@@ -88,3 +88,29 @@ $(document).ready(function () {
     }
   })
 });
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	// fullHeight();  
+
+	$(".toggle-password").click(function() {
+
+	  $(this).toggleClass("fa-eye fa-eye-slash");
+	  var input = $($(this).attr("toggle"));
+	  if (input.attr("type") == "password") {
+	    input.attr("type", "text");
+	  } else {
+	    input.attr("type", "password");
+	  }
+	});
+
+})(jQuery);
